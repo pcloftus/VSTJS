27074cff4877c9562c881a81db5b0f229a41e3e0
pcloftus <patrickcolinloftus@gmail.com>
2021-10-06 15:34:29 -0500

    Populating repo with VST_SDK base

---
+++ vst3sdk/vst3_doc/vstexamples/navtreedata.js
@@ -0,0 +1,99 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "VST 3 Examples", "index.html", [
+    [ "VST 3 plug-ins Examples", "index.html", [
+      [ "ADelay", "index.html#adelay", null ],
+      [ "AGain", "index.html#again", null ],
+      [ "TestChannelContext", "index.html#channelcontext", null ],
+      [ "HostChecker", "index.html#HostChecker", null ],
+      [ "TestLegacyMIDICCOut", "index.html#legacymidiccout", null ],
+      [ "mda Plug-ins", "index.html#mdaplugs", null ],
+      [ "Note Expression Synth", "index.html#NoteExpressionSynth", null ],
+      [ "Note Expression Text", "index.html#NoteExpressionText", null ],
+      [ "Panner", "index.html#Panner", null ],
+      [ "PitchNames", "index.html#PitchNames", null ],
+      [ "TestPrefetchableSupport", "index.html#prefetchablesupport", null ],
+      [ "TestProgramChange", "index.html#programchange", null ],
+      [ "Sync Delay", "index.html#syncdelay", null ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"VSTInterAppAudioAppDelegate_8h.html",
+"classSteinberg_1_1Vst_1_1AGain.html#a98ba8d09babc608a4d6c9566c73abf42",
+"classSteinberg_1_1Vst_1_1NoteExpressionSynth_1_1Controller.html#a4c975cd547adc38bc800fddc7f8a2954ade5bff8916b82e7c85978aee91591776",
+"classSteinberg_1_1Vst_1_1VoiceBase.html#ae9a7e45b32e54b9b41198aa3f0f033f6",
+"classSteinberg_1_1Vst_1_1mda_1_1ComboProcessor.html#af982a2d79e0dcb4d986aeb3e68181f78",
+"classSteinberg_1_1Vst_1_1mda_1_1DubDelayController.html#abbe7aa5cacfe7d13c5ba4152ac6e3b5e",
+"classSteinberg_1_1Vst_1_1mda_1_1LeslieController.html#a6d2a1929151bc7086c493054f3a7be85af908d754c50a6918f78d2bc00b5ffe55",
+"classSteinberg_1_1Vst_1_1mda_1_1ProxyParameter.html#a7d8b427caad7ea743aa02ad0dc6d9c19",
+"classSteinberg_1_1Vst_1_1mda_1_1StereoProcessor.html#a272e5d4c8e24f012573f9f5b11f7f569",
+"classVSTGUI_1_1PitchNamesDataBrowserSource.html#a11b30c6c5ebe554b06f3192ac1437347",
+"legacymidiccout_2source_2plugentry_8cpp.html",
+"mdaAmbienceController_8h.html",
+"note__expression__synth__controller_8h.html#ab04a0655cd1e3bcac5e8f48c18df1a57a77d924fdebf79683bacdc950e8d97927",
+"structSteinberg_1_1Vst_1_1mda_1_1JX10Processor_1_1VOICE.html#a00f6e6f2de2f23ec7703279c5e7c1ad6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
